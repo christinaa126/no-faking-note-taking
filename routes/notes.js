474c9414db4464a1eecd8b7456596e603b9b25d6
Christina <christinaa126@gmail.com>
2022-06-02 23:55:54 -0700

    added routes

---
+++ routes/notes.js
@@ -0,0 +1,56 @@
+const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("../helpers/fsUtils");
+
+// GET Route retrieving all notes
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route to submit notes
+notes.post("/", (req, res) => {
+  // Destructuring items in req.body
+  const { noteTitle, noteText } = req.body;
+
+  if (noteTitle && noteText) {
+    const newNote = {
+      noteTitle,
+      noteText,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json("Error in posting note");
+  }
+});
+
+// DELETE Route for a note
+notes.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes with exception of selected one
+      const result = json.filter((note) => note.id !== id);
+
+      // Save above array to the filesystem
+      writeToFile("./db/db.json", result);
+
+      res.json(`Note ${id} has been deleted `);
+    });
+});
+
+module.exports = notes;
